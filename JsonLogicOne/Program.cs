ae8f006a0e2f0038f183404a2c149b7ebfbbd38b
Salih Alper AŞKIN <alperaskin@hotmail.com>
2022-09-13 18:12:48 +0300

    initial commit

---
+++ JsonLogicOne/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JsonLogic.Net;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+var user = @"{
+    'age': '25',
+    'weight': '70',
+    'height' : '1.83',
+}";
+var rule = @"{
+        '/':[
+        {'var':'weight'},
+        {
+         '*': [
+        {'var':'height'},
+        {'var':'height'}
+        ]}
+]
+}";
+
+JToken jt = JToken.Parse(rule);
+JToken jt2 = JToken.Parse(user);
+
+JsonLogicEvaluator jsonLogic = new JsonLogicEvaluator(EvaluateOperators.Default);
+Object result = jsonLogic.Apply(jt, jt2);
+
+Console.WriteLine("Your BMI: "+ result);
